8cf8abee1352b315c2a42b05e0c513446da15285
Manukyan, Kristina <Kristina_Manukyan@epam.com>
2022-05-24 04:01:32 +0400

    Initial commit

--- string_sum.go
+++ string_sum.go
@@ -39,39 +39,36 @@ func StringSum(input string) (output string, err error) {
 	if input == "" {return "", fmt.Errorf("%w", errorEmptyInput)}
 	
 	var minus_count int
-	var plus_count int
 	s1 :=  strings.ReplaceAll(input, " ", "")
 	for _, v := range s1 {
 		if v == 45 {
 			minus_count++
-		}else if v == 43 { 
-			plus_count++
 		}
 	}
 
 	s2 := strings.FieldsFunc(s1, Split)
 	if len(s2) > 2 || len(s2) <= 1 {return "",  fmt.Errorf("%w", errorNotTwoOperands)}
+
 	opp1, err1  := strconv.Atoi(s2[0])
 	opp2, err2  := strconv.Atoi(s2[1])
+
 	if err1 != nil {
 		return "", fmt.Errorf("%w", err1)
 	}else if err2 != nil {
 		return "", fmt.Errorf("%w", err2)
 	}
+
 	if minus_count == 2 {
-		
 		return strconv.Itoa(-opp1-opp2), err
-	}else if minus_count == 1 && s1[0] == 45 {
+	}else if minus_count == 1 && s1[0] == 45 && opp1 > opp2{
 		return strconv.Itoa(opp2-opp1), err
-	}else if minus_count == 1 && s1[1] == 45  {
-		return strconv.Itoa(opp2-opp1), err
-	}else if opp1 < opp2{
+	}else if minus_count == 1 && s1[0] == 45 && opp1 < opp2{
+		return strconv.Itoa(opp1-opp2), err
+	}else if minus_count == 1 && opp1 < opp2{
+		return strconv.Itoa(opp1-opp2), err
+	}else if minus_count == 1 && opp1 > opp2{
 		return strconv.Itoa(opp1-opp2), err
 	}else {return strconv.Itoa(opp2+opp1), err}
-
-	
-
-
 	
 }
 
